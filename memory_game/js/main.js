1997a4159b6ffd54d84641449095b7c808838b23
Low Hui Yu <huiyu@Lows-MacBook-Pro.local>
2019-05-22 17:23:55 +0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,16 @@
-console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+const cards = ["queen", "queen", "king", "king"];
+const cardsInPlay = [];
+var cardOne = cards[0];
+var cardTwo = cards[1];
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
 console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+console.log("User flipped " + cardTwo);
+if(cardsInPlay.length === 2){
+	if (cardOne === cardTwo) {
+		result = alert("You found a match!");
+	} else{
+		result = alert("Sorry,try again.");
+	}
+}
+
